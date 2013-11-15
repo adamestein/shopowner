1af2f870cf805e43eecc9fd2a79b674d4d075caa
Adam Stein <adam@csh.rit.edu>
2013-11-15 16:31:53 -0500

    Added test to make sure sales home page uses the correct template and requires login

--- sales/tests.py
+++ sales/tests.py
@@ -1,9 +1,13 @@
 from django.utils import unittest
 
+from test.sales.loadpages import LoadPages
 from test.sales.navigation import NavigationTestCase
 
 def suite():
-    suite_navigation = unittest.TestLoader().loadTestsFromTestCase(NavigationTestCase)
+    loader = unittest.TestLoader()
 
-    return unittest.TestSuite([suite_navigation])
+    navigation = loader.loadTestsFromTestCase(NavigationTestCase)
+    load_pages = loader.loadTestsFromTestCase(LoadPages)
+
+    return unittest.TestSuite([navigation, load_pages])
 
