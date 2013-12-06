272ac4fcfb6b93ed1db5f04be54e6db59d95200c
Adam Stein <adam@csh.rit.edu>
2013-12-05 21:37:22 -0500

    Removed 'Update Sales Tax' link, regular users shouldn't be changing that

--- sales/navigation.py
+++ sales/navigation.py
@@ -2,16 +2,12 @@ def Navigation(on):
     navigation = (
         ["Record Sale",         "/shopowner/sales/record/"],            # index 0
         ["View Sales",          "/shopowner/sales/view/"],              # index 1
-        ["",                    ""],                                    # index 2
-        ["Update Sales Tax",    "/shopowner/sales/update/tax/"],        # index 3
     )
 
     if on == "record_sale":
         navigation[0][1] = ""
     elif on == "view_sales":
         navigation[1][1] = ""
-    elif on == "update_sales_tax":
-        navigation[3][1] = ""
 
     return navigation
 
--- test/sales/navigation.py
+++ test/sales/navigation.py
@@ -7,8 +7,6 @@ class NavigationTestCase(TestCase):
         ans = (
             ["Record Sale",         "/shopowner/sales/record/"],
             ["View Sales",          "/shopowner/sales/view/"],
-            ["",                    ""],
-            ["Update Sales Tax",    "/shopowner/sales/update/tax/"],
         )
 
         self.assertEqual(Navigation(""), ans)
@@ -17,8 +15,6 @@ class NavigationTestCase(TestCase):
         ans = (
             ["Record Sale",         ""],
             ["View Sales",          "/shopowner/sales/view/"],
-            ["",                    ""],
-            ["Update Sales Tax",    "/shopowner/sales/update/tax/"],
         )
 
         self.assertEqual(Navigation("record_sale"), ans)
@@ -27,58 +23,7 @@ class NavigationTestCase(TestCase):
         ans = (
             ["Record Sale",         "/shopowner/sales/record/"],
             ["View Sales",          ""],
-            ["",                    ""],
-            ["Update Sales Tax",    "/shopowner/sales/update/tax/"],
         )
 
         self.assertEqual(Navigation("view_sales"), ans)
-
-    def test_update_sales_tax(self):
-        ans = (
-            ["Record Sale",         "/shopowner/sales/record/"],
-            ["View Sales",          "/shopowner/sales/view/"],
-            ["",                    ""],
-            ["Update Sales Tax",    ""],
-        )
-
-        self.assertEqual(Navigation("update_sales_tax"), ans)
-
-    def footest_add_seller(self):
-        ans = (
-            ["Add Item",        "/shopowner/inventory/add/"],
-            ["Edit Item",       "/shopowner/inventory/edit/"],
-            ["List Items",      "/shopowner/inventory/list/"],
-            ["",                ""],
-            ["Add Seller",      ""],
-            ["Edit Seller",     "/shopowner/seller/edit/"],
-            ["List Sellers",    "/shopowner/seller/list/"],
-        )
-
-        self.assertEqual(Navigation("add_seller"), ans)
-
-    def footest_edit_seller(self):
-        ans = (
-            ["Add Item",        "/shopowner/inventory/add/"],
-            ["Edit Item",       "/shopowner/inventory/edit/"],
-            ["List Items",      "/shopowner/inventory/list/"],
-            ["",                ""],
-            ["Add Seller",      "/shopowner/seller/add/"],
-            ["Edit Seller",     ""],
-            ["List Sellers",    "/shopowner/seller/list/"],
-        )
-
-        self.assertEqual(Navigation("edit_seller"), ans)
-
-    def footest_list_sellers(self):
-        ans = (
-            ["Add Item",        "/shopowner/inventory/add/"],
-            ["Edit Item",       "/shopowner/inventory/edit/"],
-            ["List Items",      "/shopowner/inventory/list/"],
-            ["",                ""],
-            ["Add Seller",      "/shopowner/seller/add/"],
-            ["Edit Seller",     "/shopowner/seller/edit/"],
-            ["List Sellers",    ""],
-        )
-
-        self.assertEqual(Navigation("list_sellers"), ans)
 
