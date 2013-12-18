c7a9bb4fc45aac1a92de43cb6c1b4c09a3f0dbf5
Adam Stein <adam@csh.rit.edu>
2013-12-18 10:07:21 -0500

    Updated version number

--- test/common/cp.py
+++ test/common/cp.py
@@ -5,7 +5,7 @@ from common.context_processors import version
 class CPTestCase(TestCase):
     def test_version(self):
         ans = {
-            "version": "Shop Owner Apps v1.7.1"
+            "version": "Shop Owner Apps v1.7.2"
         }
 
         self.assertEqual(version(None), ans)
